b3dcbc48e236bb3bf350a764cf5269543ae7d48c
Paolo Gajo <paolo.gajo@studio.unibo.it>
2024-05-23 14:11:54 +0200

    fix prompt typo

--- api/src/components/question_proposal_generator.py
+++ api/src/components/question_proposal_generator.py
@@ -17,7 +17,7 @@ class QuestionProposalGenerator(BaseComponent):
 
     def get_system_message(self) -> str:
         system = f"""
-        Your task is to come up with questions someone might as about the content of a Neo4j database. Try to make the questions as different as possible.
+        Your task is to come up with questions someone might ask about the content of a Neo4j database. Try to make the questions as different as possible.
         The questions should be separated by a new line and each line should only contain one question.
         To do this, you need to understand the schema of the database. Therefore it's very important that you read the schema carefully. You can find the schema below.
         Schema: 
